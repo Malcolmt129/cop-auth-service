6e0e63838e913a1763db1ddc8abbf415ff462f34
Cwagne17 <cwagne17@students.towson.edu>
2022-04-26 21:37:23 -0400

    add default route

---
+++ src/app.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+
+    @Get() 
+    getVersion() {
+        return { message: `Welcome to the Church of Phillippi Auth Service Version ${require('../../package.json').versionsion}`};
+    }
+}

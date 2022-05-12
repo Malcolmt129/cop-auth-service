98c2d4b2d0d63f1705b918c9793c9c9e719307a3
Cwagne17 <cwagne17@students.towson.edu>
2022-05-11 20:51:59 -0400

    change route

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,4 @@
-import { Body, Controller, Get, HttpCode, Param, Patch, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { Roles } from '../auth/roles.decorator';
-import { UpdateUserRoleDto, UserRole } from './dto/update-user.dto';
+import { Controller, Get, HttpCode } from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
@@ -10,18 +7,9 @@ export class UserController {
     constructor(private readonly userService: UserService) {}
 
     @Get('/users')
-    @UseGuards(JwtAuthGuard)
     @HttpCode(200)
     async getAllUsers(): Promise<Partial<User[]>> {
         return await this.userService.findAllUsers();
-    }
-
-    @Patch('/user/:userId/role')
-    @UseGuards(JwtAuthGuard)
-    @Roles(UserRole.ADMIN)
-    @HttpCode(204)
-    async updateUserRole(@Param('userId') id: string, @Body() updateUserRoleDto: UpdateUserRoleDto): Promise<void> {
-        await this.userService.updateUserRole(id, updateUserRoleDto);
     }
 
 }
